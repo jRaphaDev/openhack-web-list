a135115aad9c0d758f14e8eb335932cc7f0b4c0f
Raphael Freitas <dev.rapha@gmail.com>
2018-10-25 09:03:55 -0300

    start project

---
+++ src/components/servers/Server.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Server extends Component {
+  render() {
+    return (
+      <div className="Server">
+        Server.
+      </div>
+    );
+  }
+}
